2ae5536bbc53d0f68edc3c7c0453453b6e968d8b
Stijn Caerts <stijn.caerts@vito.be>
2023-11-15 15:34:19 +0100

    fix running pre_test script as jenkins user

--- tests/pre_test.sh
+++ tests/pre_test.sh
@@ -2,18 +2,19 @@ ES=elasticsearch-8.11.1
 ES_ARCHIVE=$ES-linux-x86_64.tar.gz
 ES_ARCHIVE_SHA512=$ES_ARCHIVE.sha512
 
-set -e
-su jenkins  # elasticsearch fails to start when running as root
-whoami
-cd /tmp
+# elasticsearch fails to start when running as root
+su jenkins -c '
+    set -e
+    cd /tmp
 
-curl -O $ES_ARCHIVE https://artifacts.elastic.co/downloads/elasticsearch/$ES_ARCHIVE
-curl -O $ES_ARCHIVE_SHA512 https://artifacts.elastic.co/downloads/elasticsearch/$ES_ARCHIVE_SHA512
+    curl -O $ES_ARCHIVE https://artifacts.elastic.co/downloads/elasticsearch/$ES_ARCHIVE
+    curl -O $ES_ARCHIVE_SHA512 https://artifacts.elastic.co/downloads/elasticsearch/$ES_ARCHIVE_SHA512
 
-sha512sum -c $ES_ARCHIVE_SHA512
-tar -xzf $ES_ARCHIVE
+    sha512sum -c $ES_ARCHIVE_SHA512
+    tar -xzf $ES_ARCHIVE
 
-# disable security
-sed -i 's/xpack.security.enabled: true/xpack.security.enabled: false/g' $ES/config/elasticsearch.yml
-export ELASTIC_XPACK_SECURITY_ENABLED=false
-./$ES/bin/elasticsearch -d -p $ES.pid
+    # disable security
+    sed -i "s/xpack.security.enabled: true/xpack.security.enabled: false/g" $ES/config/elasticsearch.yml
+    export ELASTIC_XPACK_SECURITY_ENABLED=false
+    ./$ES/bin/elasticsearch -d -p $ES.pid
+'

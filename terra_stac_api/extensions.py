deca3612c8a4f970c087f8e475b8ed5087f07c44
Stijn Caerts <stijn.caerts@vito.be>
2026-09-24 15:49:41 +0200

    fix: override transaction extension with auth support

--- terra_stac_api/extensions.py
+++ terra_stac_api/extensions.py
@@ -2,11 +2,16 @@ from stac_fastapi.core.extensions.aggregation import (
     EsAggregationExtensionGetRequest,
     EsAggregationExtensionPostRequest,
 )
-from stac_fastapi.extensions import AggregationExtension
+from stac_fastapi.extensions import (
+    AggregationExtension,
+    BulkTransactionExtension,
+    TransactionExtension,
+)
 from stac_fastapi.sfeos_helpers.models.extensions import Extensions
 from stac_fastapi.types.extension import ApiExtension
 
 from terra_stac_api.aggregation_client import AggregationClientAuth
+from terra_stac_api.core import BulkTransactionsClientAuth, TransactionsClientAuth
 
 
 class ExtensionsAuth(Extensions):
@@ -14,6 +19,9 @@ class ExtensionsAuth(Extensions):
 
     @property
     def aggregation(self) -> list[ApiExtension]:
+        """
+        Return the aggregation extension with authorization checks.
+        """
         aggregation_extension = AggregationExtension(
             client=AggregationClientAuth(
                 database=self.database_logic,
@@ -25,3 +33,29 @@ class ExtensionsAuth(Extensions):
         aggregation_extension.POST = EsAggregationExtensionPostRequest
 
         return [aggregation_extension]
+
+    @property
+    def transaction(self) -> list[ApiExtension]:
+        """
+        Return the transaction extension with authorization checks.
+        """
+        if not self.transactions_enabled:
+            return []
+
+        return [
+            TransactionExtension(
+                client=TransactionsClientAuth(
+                    database=self.database_logic,
+                    session=self.session,
+                    settings=self.settings,
+                ),
+                settings=self.settings,
+            ),
+            BulkTransactionExtension(
+                client=BulkTransactionsClientAuth(
+                    database=self.database_logic,
+                    session=self.session,
+                    settings=self.settings,
+                )
+            ),
+        ]
